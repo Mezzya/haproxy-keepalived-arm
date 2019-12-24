96a4a39fea8bb34c4ee6d6527b559e1531e8b111
Andrey Megeria <andrey.megeria@gmail.com>
2019-12-24 12:32:14 +0200

    fix timeout curl error

--- keepalived-check-appsvc.sh
+++ keepalived-check-appsvc.sh
@@ -2,7 +2,7 @@
 
 URL="http://localhost"
 
-if [[ `curl -s -o/dev/null --connect-timeout 0.5 $URL; echo $?` -ne 0 ]]; then
+if [[ `curl -s -o/dev/null --connect-timeout 1 $URL; echo $?` -ne 0 ]]; then
     exit 1
 else
     exit 0
