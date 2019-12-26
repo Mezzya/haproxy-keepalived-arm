094a15d66413b6e3c7185325cc25334e2a49886a
Andrey Megeria <andrey.megeria@gmail.com>
2019-12-26 12:35:04 +0200

    fix

--- haproxyvm-configure.sh
+++ haproxyvm-configure.sh
@@ -63,7 +63,7 @@ setup_haproxy() {
     systemctl daemon-reload
     chkconfig haproxy on
     useradd -r haproxy
-    cd ~
+    cd -
     # Enable haproxy (to be started during boot)
     # tmpf=`mktemp` && mv /etc/default/haproxy $tmpf && sed -e "s/ENABLED=0/ENABLED=1/" $tmpf > /etc/default/haproxy && chmod --reference $tmpf /etc/default/haproxy
 
@@ -132,7 +132,7 @@ setup_keepalived() {
     cd keepalived-2.0.19
     ./configure --prefix=/; make; make install
     chkconfig keepalived on
-    cd ~
+    cd -
 
     # Setup keepalived.conf
     KEEPALIVED_CFG=/etc/keepalived/keepalived.conf
